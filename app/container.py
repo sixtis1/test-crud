4fdb18e9ff4beabdaf7f48b13fcb6f6b2b73dd2e
sixtis1 <asdw2001@gmail.com>
2024-12-10 19:45:56 +0600

    feat: added cli for user

--- app/container.py
+++ app/container.py
@@ -1,15 +1,13 @@
 import punq
 
 from app.base.place_of_work.storage.base.base import PlaceOfWorkRepository
-from app.base.place_of_work.storage.memory.memory_repository import (
-    MemoryPlaceOfWorkRepository,
-)
-from app.base.place_of_work.storage.postgres.db_repository import (
-    DBPlaceOfWorkRepository,
-)
+from app.base.place_of_work.storage.memory.memory_repository import MemoryPlaceOfWorkRepository
+from app.base.place_of_work.storage.postgres.db_repository import DBPlaceOfWorkRepository
+
 from app.base.user.storage.base.base import UserRepository
 from app.base.user.storage.memory.memory_repository import MemoryUserRepository
 from app.base.user.storage.postgres.db_repository import DBUserRepository
+
 from app.config import app_settings
 from app.base.user.storage.base.session_factory import SessionFactory
 
@@ -22,11 +20,12 @@ if repository_type == "postgres":
     container.register(SessionFactory, instance=session_factory)
 
     container.register(
-        UserRepository, factory=lambda: DBUserRepository(session_factory.get_session())
+        UserRepository,
+        factory=lambda: DBUserRepository(session_factory.get_session_maker())
     )
     container.register(
         PlaceOfWorkRepository,
-        factory=lambda: DBPlaceOfWorkRepository(session_factory.get_session()),
+        factory=lambda: DBPlaceOfWorkRepository(session_factory.get_session_maker()),
     )
 else:
     container.register(UserRepository, MemoryUserRepository)
